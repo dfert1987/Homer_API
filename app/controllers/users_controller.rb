78069ee6e651ebb95f94c43ed9b87d827c41fe1e
David Fertitta <davidj.fertitta@gmail.com>
2021-07-27 10:11:24 -0600

    user controller basic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def create
+        @user = User.create user_params
+        render json: @user
+    end
+
+    def update
+        @user = User.update update_params
+        render json: @user
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password, :email)
+    end
+
+    def update_params
+        params.require(:user).permit(:username, :password_digest, :email, :avatar, :twitter, :dob, :about, :name, :city, :state)
+    end
+    
 end
